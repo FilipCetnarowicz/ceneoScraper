0b452fa93b74843e52038eae97f0e32d7d0cddb0
FilipCetnarowicz <fcetnarowicz@gmail.com>
2024-04-27 11:03:23 +0200

    FC

---
+++ xxx.py
@@ -0,0 +1,16 @@
+import requests as re 
+from bs4 import BeautifulSoup
+id = "99069353"
+url = "https://www.ceneo.pl/99069353#tab=reviews_scroll"
+#url = "https://www.ceneo.pl/"
+
+http = re.get(url)
+
+print(http.status_code)
+#print(http.text)
+
+page = BeautifulSoup(http.text,"html.parser")
+# options = page.select
+print(page.select_one("div.js_product-reviews"))
+
+    
